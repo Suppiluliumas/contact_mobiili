e270c00c1e42239e47251f60dc7a4c67a8b24ed0
Suppiluliuma <jesse.nevalainen@myy.haaga-helia.fi>
2023-10-07 00:19:37 +0300

    valmis

--- App.js
+++ App.js
@@ -1,10 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as Contacts from "expo-contacts";
+import { useState } from "react";
+import { Button } from "react-native";
+import { FlatList } from "react-native";
 
 export default function App() {
+  const [contacts, setContacts] = useState([]);
+  const getContacts = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+    if (status === "granted") {
+      const { data } = await Contacts.getContactsAsync({
+        fields: [Contacts.Fields.Name, Contacts.Fields.PhoneNumbers],
+      });
+      {
+      }
+      if (data.length > 0) {
+        setContacts(data);
+      }
+    }
+    
+  };
+  console.log(contacts);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList
+        data={contacts}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => {
+          if (item.phoneNumbers && item.phoneNumbers[0]?.number !== "") {
+            return (
+              <View style={styles.item}>
+                <Text style={styles.name}>{item.name} {item.phoneNumbers[0].number}</Text>
+              </View>
+            );
+          }
+
+          return (
+            <View style={styles.item}>
+            <Text>{item.name} Ei numeroa</Text>
+            </View>
+          );
+        }}
+      />
+      <Button title="Get Contacts" onPress={getContacts} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +52,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  item: {
+    borderBottomWidth: 1,
+    borderBottomColor: "lightgray",
+    padding: 16,
   },
 });
